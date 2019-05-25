e90446532f1ac5a6030e199908d5f74469a68b55
Trân Dương <dtran2108@gmail.com>
2019-05-25 16:58:05 +0700

    add models

---
+++ FarCryOnlinePlatForm/far_cry_online_platform/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField()
+    password = models.CharField()
+
+
+class User_Settings(models.Model):
+    HERO = 'hero'
+    MERC_SCOUT = 'merc_scout'
+    MERC_REAR = 'merc_rear'
+    EVIL_WORKER = 'evil_worker'
+    MERTZ = 'mertz'
+    model_choices = [
+        (HERO, "objects/characters/pmodels/hero/hero_mp.cgf"),
+        (MERC_REAR, "objects/characters/mercenaries/merc_scout/merc_scout_mp.cgf"),
+        (MERC_REAR, "objects/characters/mercenaries/merc_rear/merc_rear_mp.cgf"),
+        (EVIL_WORKER, "bbjects/characters/workers/evil_worker/evil_worker_MP.cgf"),
+        (MERTZ, "objects/characters/pmodels/story_characters/mertz/mertz_mp.cgf")
+    ]
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    screen_quality = models.IntegerField()
+    lazy_weapon = models.DecimalField()
+    brightness = models.DecimalField()
+    model = models.CharField(choices=model_choices, default=HERO)
+    player_skin = models.IntegerField()
+
+
+class Match(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class Match_frag(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    frag_time = models.DateField()
+    killer_name = models.ForeignKey(User)
+    victim_name = models.ForeignKey(User)
+    weapon_code = models.CharField
+
+
+

48abaec6a1f0f9cf0a1f7227b457aa7bc875aa47
dtran00 <adfa@afda.com>
2019-06-03 17:59:02 +0700

    api

--- back-end/accounts/serializers.py
+++ back-end/accounts/serializers.py
@@ -3,15 +3,7 @@ from django.contrib.auth.models import User
 from .models import UserSettings
 from django.contrib.auth import authenticate
 
-# user serializer
-class UserSerializer(serializers.ModelSerializer):
-    settings = serializers.StringRelatedField()
-    match_frags_killer = serializers.SlugRelatedField(many=True, read_only=True, slug_field='killer')
-    match_frags_victime = serializers.SlugRelatedField(many=True, read_only=True, slug_field='victime')
 
-    class Meta:
-        model = User
-        fields = ('id','username','email', 'settings', 'match_frags')
     
 
 # register/signup
@@ -42,7 +34,22 @@ class UserSettingSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = UserSettings
-        fields = ('user', 'screen_quality', 'lazy_weapon', 'brightness', 'model', 'player_skin')
+        fields = (
+            'user',
+            'screen_quality',
+            'lazy_weapon',
+            'brightness',
+            'model',
+            'ingame_name',
+            'player_skin',
+            'k_move_left',
+            'k_move_right',
+            'k_move_forward',
+            'k_move_backward',
+            'k_reload',
+            'k_movemode2',
+            'k_firemode'
+            )
     
     def create(self, validated_data):
         user = self.context['request'].user
@@ -51,7 +58,27 @@ class UserSettingSerializer(serializers.ModelSerializer):
     
     def update(self, instance, validated_data):
         instance.screen_quality = validated_data['screen_quality']
+        instance.lazy_weapon = validated_data['lazy_weapon']
+        instance.brightness = validated_data['brightness']
+        instance.model = validated_data['model']
+        instance.player_skin = validated_data['player_skin']
+        instance.ingame_name = validated_data['ingame_name']
+
+        instance.k_move_left = validated_data['k_move_left']
+        instance.k_move_right = validated_data['k_move_right']
+        instance.k_move_forward = validated_data['k_move_forward']
+        instance.k_move_backward = validated_data['k_move_backward']
+        instance.k_reload = validated_data['k_reload']
+        instance.k_movemode2 = validated_data['k_movemode2']
+        instance.k_firemode = validated_data['k_firemode']
         user = self.context['request'].user
         instance.user = user
         return instance
 
+# user serializer
+class UserSerializer(serializers.ModelSerializer):
+    settings = UserSettingSerializer()
+
+    class Meta:
+        model = User
+        fields = ('id','username','email', 'settings')

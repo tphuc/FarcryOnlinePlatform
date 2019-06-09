4c7df4253034c1e49f64e67dcf87cd7954eba892
tphuc <phuccokerml@gmail.com>
2019-06-09 20:18:51 +0700

    update requirements

--- FarCryOnlinePlatForm/settings.py
+++ FarCryOnlinePlatForm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -136,3 +136,4 @@ STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+django_heroku.settings(locals())
